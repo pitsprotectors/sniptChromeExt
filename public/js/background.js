b0618c382d17743670b5a3a4fb5bf0a1b16ebcac
Ding Pang <dpang@middlebury.edu>
2019-08-15 17:33:07 -0400

    added selecting question and saving tto exsisting project

--- public/js/background.js
+++ public/js/background.js
@@ -1,14 +1,16 @@
-// chrome.runtime.onInstalled.addListener(function() {
-//   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-//     chrome.declarativeContent.onPageChanged.addRules([
-//       {
-//         conditions: [
-//           new chrome.declarativeContent.PageStateMatcher({
-//             pageUrl: { urlPrefix: "" }
-//           })
-//         ],
-//         actions: [new chrome.declarativeContent.ShowPageAction()]
-//       }
-//     ]);
-//   });
-// });
+console.log('background running');
+
+
+let contextMenuItem ={
+  "id": "add snipt",
+  "title": "Add this snipt",
+  "contexts":["selection"]
+}
+chrome.contextMenus.create(contextMenuItem)
+window.snipts=[]
+
+chrome.contextMenus.onClicked.addListener(function(clickData){
+  if (clickData.menuItemId==="add snipt" && clickData.selectionText){
+    window.snipts.push(clickData.selectionText)
+  }
+})
